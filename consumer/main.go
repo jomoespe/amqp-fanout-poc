d829ee2319ff11225595bea1c898880f3bdac42d
Jose <jomoespe@gmail.com>
2022-03-17 13:33:21 +0100

    Declare exchange on startup time and generate queue name

--- consumer/main.go
+++ consumer/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/google/uuid"
 	"github.com/streadway/amqp"
 )
 
-const exchange = "poc.messages"
+const exchangeName = "poc.messages"
 
 func main() {
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
@@ -17,24 +19,35 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
+	err = ch.ExchangeDeclare(
+		exchangeName, // name
+		"fanout",     // kind
+		true,         // durable
+		false,        // auto-delete
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, fmt.Sprintf("Failed to declare the exchange %s", exchangeName))
+
 	q, err := ch.QueueDeclare(
-		"",    // name. Queue name empty will generate a unique name which will be returned in the Name field
-		false, // durable
-		true,  // delete when unused
-		false, // exclusive
-		false, // no-wait
-		nil,   // arguments
+		queueName(), // name. Queue name empty will generate a unique name which will be returned in the Name field
+		false,       // durable
+		true,        // delete when unused
+		false,       // exclusive
+		false,       // no-wait
+		nil,         // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
 	err = ch.QueueBind(
-		q.Name,   // name
-		"",       // key
-		exchange, // exchange
-		false,    // no-wait
-		nil,      // arguments
+		q.Name,       // name
+		"",           // key
+		exchangeName, // exchange
+		false,        // no-wait
+		nil,          // arguments
 	)
-	failOnError(err, "Failed to bind to a queue "+q.Name)
+	failOnError(err, fmt.Sprintf("Failed to bind the queue %s to exchange %s", q.Name, exchangeName))
 
 	msgs, err := ch.Consume(
 		q.Name, // queue
@@ -45,7 +58,7 @@ func main() {
 		false,  // no-wait
 		nil,    // args
 	)
-	failOnError(err, "Failed to register a consumer to queue "+q.Name)
+	failOnError(err, fmt.Sprintf("Failed to register a consumer to queue %s", q.Name))
 
 	forever := make(chan bool)
 
@@ -55,9 +68,13 @@ func main() {
 		}
 	}()
 
-	log.Printf("Queue name: %s", q.Name)
+	log.Printf("Queue name: %s bound to %s", q.Name, exchangeName)
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
 	<-forever
+}
+
+func queueName() string {
+	return fmt.Sprintf("%s@%s", uuid.NewString(), exchangeName)
 }
 
 func failOnError(err error, msg string) {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jomoespe/amqp-fanout-poc
 
 go 1.17
 
-require github.com/streadway/amqp v1.0.0
+require (
+	github.com/google/uuid v1.3.0
+	github.com/streadway/amqp v1.0.0
+)
